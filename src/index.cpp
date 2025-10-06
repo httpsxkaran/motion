8b45a698ea994dd7a2f0dd37f874430f5160d4ef
Karany24 <ky334219@gmail.com>
2025-10-06 22:37:07 +0530

    added

---
+++ src/index.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    vector<int> vec;
+    int a;
+    int avg = 0, sum = 0;
+    int small = INT8_MAX;
+    int max = INT8_MIN;
+    cout<<"Enter the number of inputs"<<endl;
+    cin>>a;
+    vec.resize(a);
+    for(int i = 0; i < a ; i++ ){
+        cin>>vec[i];
+    }
+    for(int i = 0; i < a ; i++ ){
+         if(i % 2 == 0)
+         cout<<vec[i]<<" ";
+    }
+    
+    
+}
