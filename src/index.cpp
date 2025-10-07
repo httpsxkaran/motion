7027d17c36e0b8a390dd5686622f42cc2be82d9d
Karany24 <ky334219@gmail.com>
2025-10-07 21:50:38 +0530

    added

--- src/index.cpp
+++ src/index.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(){
     vector<int> vec;
     int a;
-    int avg = 0, sum = 0;
+    int avg = 0, res = 1, sum = 0;
     int small = INT8_MAX;
     int max = INT8_MIN;
     cout<<"Enter the number of inputs"<<endl;
@@ -14,10 +14,10 @@ int main(){
     for(int i = 0; i < a ; i++ ){
         cin>>vec[i];
     }
-    for(int i = 0; i < a ; i++ ){
-         if(i % 2 == 0)
-         cout<<vec[i]<<" ";
+    for(int i = a/2; i < a ; i++ ){
+       sum += vec[i];
     }
+    cout<<sum;
     //
     ///
     
