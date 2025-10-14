38c5f6bfee2d07f14c6987f9b4520c767e382fcb
Karany24 <ky334219@gmail.com>
2025-10-14 23:39:42 +0530

    removed some bugs

--- src/index.cpp
+++ src/index.cpp
@@ -5,38 +5,33 @@ using namespace std;
 int main(){
     vector<int> vec;
     int a, res = 0, res1 = 0, min = INT8_MAX ;
-    int avg = 0, sum = 0;
-    int count = 0;
+    int avg = 1, sum = 0;
+    int count = 1;
     int small = INT8_MAX;
     int b,max = INT8_MIN;
     cout<<"Enter the number of inputs"<<endl;
     cin>>a;
     vec.resize(a);
 
-    for(int i = 0; i < a ; i++ ){///
+    for(int i = 0; i < a ; i++ ){
         cin>>vec[i];
     }
 
-    
-    for(int i = 0 ; i < a ; i++){
-        for(int j = 0 ; j< a ; j++){
-           if(vec[i]==vec[j])
-           count++;
+   for(int i = 0; i<a ; i++){
+    sum = vec[i];
+    for(int j = i+1; j< a ; j++){
+        sum +=vec[j];
+        if(sum%2==0){
+            
+              for( int k = i;k<=j; k++){
+                cout<<vec[k]<<" ";
+                
+              }
+              cout<<endl;
         }
-        if(count>1){
-                 
-            res = count;
-           
-        }
-         if(res < min && count>1){
-            min = count; 
-            res1 = vec[i];
-        }
-        
-        count=0;
     }
-    if(res1==0)
-    cout<<"-1";
-    else cout<<res1;
+   
+   } 
+ 
     
 }
